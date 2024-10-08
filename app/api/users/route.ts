4c4acd59cebba2961f3809abd14731ff9b33e8c9
Yil-Verdeja <codecooker@gmail.com>
2024-10-09 02:51:19 +0800

    feat: setup get user route

--- app/api/users/route.ts
+++ app/api/users/route.ts
@@ -1,7 +1,8 @@
-import { sql } from "@vercel/postgres";
+import db from "@/lib/postgres-kysely/db";
 import { NextResponse } from "next/server";
 
 export async function GET(request: Request) {
-	const users = await sql`SELECT * FROM Users;`;
+	// const users = await sql`SELECT * FROM Users;`;
+	const users = await db.selectFrom("users").selectAll().execute();
 	return NextResponse.json({ users }, { status: 200 });
 }
