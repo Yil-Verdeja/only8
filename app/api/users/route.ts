dc4acfb6572a7cd6977dc24269c79a02e1ed67cf
Yil-Verdeja <codecooker@gmail.com>
2024-10-09 03:24:50 +0800

    feat: add account managers route

--- app/api/users/route.ts
+++ app/api/users/route.ts
@@ -2,7 +2,6 @@ import db from "@/lib/postgres-kysely/db";
 import { NextResponse } from "next/server";
 
 export async function GET(request: Request) {
-	// const users = await sql`SELECT * FROM Users;`;
 	const users = await db.selectFrom("users").selectAll().execute();
 	return NextResponse.json({ users }, { status: 200 });
 }
